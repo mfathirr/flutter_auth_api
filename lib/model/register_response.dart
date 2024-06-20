4efb3fb7d69ac68cd39cf9170ca0225d815decf1
mfathirr <mulfathir632@gmail.com>
2024-06-20 09:32:20 +0700

    create register response model

---
+++ lib/model/register_response.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+class RegisterResponse {
+  bool error;
+  String message;
+
+  RegisterResponse({
+    required this.error,
+    required this.message,
+  });
+
+  factory RegisterResponse.fromJson(String str) =>
+      RegisterResponse.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory RegisterResponse.fromMap(Map<String, dynamic> json) =>
+      RegisterResponse(
+        error: json["error"],
+        message: json["message"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "error": error,
+        "message": message,
+      };
+}
